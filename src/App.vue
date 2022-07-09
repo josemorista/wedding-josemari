6fe2a4878b815a9939164410f222445804d0ed8c
josemorista <josemorista@id.uff.br>
2022-07-08 22:15:20 -0300

    feat: initial commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <NavBar />
+  <section class="hero">
+
+  </section>
+</template>
+
+<script setup lang="ts">
+import NavBar from "./components/NavBar/NavBar.vue";
+</script>
+
+<style src="./styles/app.scss">
+</style>
---
+++ src/components/NavBar/NavBar.vue
@@ -0,0 +1,28 @@
+<template>
+	<nav class="nav-bar">
+		<div class="names-and-date">
+			<h1>
+				Casamento José Morista e Mariane Ferreira
+			</h1>
+			<h4>
+				12/03/2023
+			</h4>
+		</div>
+		<div class="nav-links">
+			<ul>
+				<li v-for="link of navLinks" :key="link">
+					<a class="nav-link" href="">{{ link }}</a>
+				</li>
+			</ul>
+		</div>
+	</nav>
+</template>
+
+<script setup lang="ts">
+const navLinks = [
+	"HOME", "OS NOIVOS", "PADRINHOS", "CERIMÔNIA", "RECEPÇÃO", "LISTA DE PRESENTES", "CONFIRME SUA PRESENÇA", "RECADOS"
+];
+</script>
+
+<style src="./NavBar.scss">
+</style>
